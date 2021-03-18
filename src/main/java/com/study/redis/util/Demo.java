96349159cbac10cb3cccc7fe57612bfed978f947
LiuTalkweb <809489286@qq.ocm>
2021-03-18 11:27:12 +0800

    test

---
+++ src/main/java/com/study/redis/util/Demo.java
@@ -0,0 +1,13 @@
+package com.study.redis.util;
+
+/**
+ * @program: redis-study
+ * @description:
+ * @author: liuB
+ * @create: 2021-03-18 11:25
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
